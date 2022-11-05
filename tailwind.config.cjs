4e25404e0cdf52b335bd54ec3dc13221e6e108d9
MarcelRG <mrodrigonz@gmail.com>
2022-11-05 00:17:00 -0600

    Building contact component

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,6 +8,25 @@ module.exports = {
       fontFamily: {
         sans: ["Lexend Deca", ...defaultTheme.fontFamily.sans],
       },
+      animation: {
+        blob: "blob 7s infinite",
+      },
+      keyframes: {
+        blob: {
+          "0%": {
+            transform: "scale(1)",
+          },
+          "33%": {
+            transform: "scale(1.1)",
+          },
+          "66%": {
+            transform: "scale(0.9)",
+          },
+          "100%": {
+            transform: "scale(1)",
+          },
+        },
+      },
     },
   },
   plugins: [require("@tailwindcss/typography")],
